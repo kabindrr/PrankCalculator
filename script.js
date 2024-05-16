ca355704015a5d0784f6c29635b848a3e7e1cc68
kabindrr <chaina_kabindra@yahoo.com>
2024-05-16 12:09:30 +1000

    handle last operator

--- script.js
+++ script.js
@@ -1,27 +1,134 @@
-// const clickAllButtons = document.querySelectorAll(".btn");
-// console.log(clickAllButtons);
+// // const clickAllButtons = document.querySelectorAll(".btn");
+// // console.log(clickAllButtons);
+
+// // let strToDisplay = "";
+// // const displayElm = document.querySelector(".display");
+
+// // const buttonAction = (value) => {
+// //   console.log(value);
+// //   if (value === "AC") {
+// //     strToDisplay = "";
+// //     return display(strToDisplay);
+// //   }
+// //   if (value === "=") {
+// //     return displayTotal();
+// //   }
+
+// //   strToDisplay += value;
+// //   display(strToDisplay);
+// // };
+
+// // clickAllButtons.forEach((btn) => {
+// //   btn.addEventListener("click", () => {
+// //     const value = btn.innerText;
+// //     buttonAction(value);
+// //   });
+// // });
+
+// // const display = (str) => {
+// //   displayElm.innerText = str || "0.0";
+// // };
+
+// // const displayTotal = () => {
+// //   const total = eval(strToDisplay);
+// //   display(total);
+// // };
+
+// const getButtonClicked = document.querySelectorAll(".btn");
+// console.log(getButtonClicked);
 
 // let strToDisplay = "";
 // const displayElm = document.querySelector(".display");
+// console.log(displayElm);
+
+// const operators = ["+", "-", "/", "*", "%"];
 
 // const buttonAction = (value) => {
-//   console.log(value);
 //   if (value === "AC") {
 //     strToDisplay = "";
 //     return display(strToDisplay);
 //   }
+//   if (value === "C") {
+//     strToDisplay = strToDisplay.slice(0, -1);
+//     return display(strToDisplay);
+//   }
+
 //   if (value === "=") {
-//     return displayTotal();
+//     const lastCharacter = strToDisplay[strToDisplay.length - 1];
+//     if (operators.includes(lastCharacter)) {
+//       strToDisplay = strToDisplay.slice(0, -1);
+//     }
+//     return getTotal();
 //   }
 
 //   strToDisplay += value;
 //   display(strToDisplay);
 // };
 
-// clickAllButtons.forEach((btn) => {
+// getButtonClicked.forEach((btn) => {
 //   btn.addEventListener("click", () => {
 //     const value = btn.innerText;
 //     buttonAction(value);
+//   });
+// });
+// const display = (str) => {
+//   displayElm.innerText = str || "0.0";
+// };
+// const getTotal = () => {
+//   const total = eval(strToDisplay);
+//   strToDisplay = total.toString();
+//   display(strToDisplay);
+// };
+
+// const getAllButtonsClicked = document.querySelectorAll(".btn");
+
+// let strToDisplay = "";
+// const displayElm = document.querySelector(".display");
+
+// const operators = ["+", "-", "*", "/", "%"];
+
+// const buttonAction = (main) => {
+//   if (main === "AC") {
+//     strToDisplay = "";
+//     return display(strToDisplay);
+//   }
+
+//   if (main === "C") {
+//     strToDisplay = strToDisplay.slice(0, -1);
+//     return display(strToDisplay);
+//   }
+
+//   if (main === "=") {
+//     const lastChar = strToDisplay[strToDisplay.length - 1];
+//     if (operators.includes(lastChar)) {
+//       strToDisplay = strToDisplay.slice(0, -1);
+//     }
+//     return getCalculations();
+//   }
+
+//   // if (operators.includes(main)) {
+//   //   const lastChar = strToDisplay[strToDisplay.length - 1];
+
+//   //   if (operators.includes(lastChar)) {
+//   //     strToDisplay = strToDisplay.slice(0, -1);
+//   //   }
+//   // }
+
+//   if (operators.includes(main)) {
+//     const lastChar = strToDisplay[strToDisplay.length - 1];
+//     if (operators.includes(lastChar)) {
+//       strToDisplay = strToDisplay.slice(0, -1);
+//     }
+//   }
+
+//   strToDisplay += main;
+//   display(strToDisplay);
+// };
+
+// getAllButtonsClicked.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const main = btn.innerText;
+//     buttonAction(main);
 //   });
 // });
 
@@ -29,34 +136,53 @@
 //   displayElm.innerText = str || "0.0";
 // };
 
-// const displayTotal = () => {
+// const getCalculations = () => {
 //   const total = eval(strToDisplay);
-//   display(total);
+//   strToDisplay = total.toString();
+//   display(strToDisplay);
 // };
 
-const getButtonsClicked = document.querySelectorAll(".btn");
+const getAllButtonsClicked = document.querySelectorAll(".btn");
 
 let strToDisplay = "";
 const displayElm = document.querySelector(".display");
 
-const buttonAction = (main) => {
-  if (main === "AC") {
+const operators = ["+", "-", "/", "*", "%"];
+
+const buttonAction = (value) => {
+  if (value === "AC") {
     strToDisplay = "";
     return display(strToDisplay);
   }
 
-  if (main === "=") {
-    return getTotal();
+  if (value === "C") {
+    strToDisplay = strToDisplay.slice(0, -1);
+    return display(strToDisplay);
   }
 
-  strToDisplay += main;
+  if (value === "=") {
+    const lastCharacter = strToDisplay[strToDisplay.length - 1];
+    if (operators.includes(lastCharacter)) {
+      strToDisplay = strToDisplay.slice(0, -1);
+    }
+    return getCalculations();
+  }
+
+  if (operators.includes(value)) {
+    const lastChar = strToDisplay[strToDisplay.length - 1];
+    if (operators.includes(lastChar)) {
+      strToDisplay = strToDisplay.slice(0, -1);
+    }
+  }
+
+  strToDisplay += value;
   display(strToDisplay);
 };
 
-getButtonsClicked.forEach((btn) => {
+getAllButtonsClicked.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const main = btn.innerText;
-    buttonAction(main);
+    const value = btn.innerText;
+    buttonAction(value);
   });
 });
 
@@ -64,7 +190,8 @@ const display = (str) => {
   displayElm.innerText = str || "0.0";
 };
 
-const getTotal = () => {
+const getCalculations = () => {
   const total = eval(strToDisplay);
-  display(total);
+  strToDisplay = total.toString();
+  display(strToDisplay);
 };
